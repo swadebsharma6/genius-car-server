9d48323e1e405e245df8fcabb6af7af7f12cfbf9
swadebsharma6 <swadebriva2012@gmail.com>
2024-02-04 17:27:34 +0600

    added orders post and get apis

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,7 @@ async function run() {
 
     const serviceCollection = client.db('GeniusCarDB').collection('services');
     const productCollection = client.db('GeniusCarDB').collection('products');
+    const orderCollection = client.db('GeniusCarDB').collection('orders');
 
     app.get('/services', async(req, res)=>{
         const cursor = serviceCollection.find();
@@ -35,10 +36,44 @@ async function run() {
         res.send(result);
     });
 
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query ={_id : new ObjectId(id)};
+      const result = await serviceCollection.findOne(query);
+      res.send(result)
+    })
+
     app.get('/products', async(req, res)=>{
       const cursor = productCollection.find();
       const result = await cursor.toArray();
       res.send(result)
+    });
+
+    // Orders Api
+    app.get('/orders', async(req, res)=>{
+      let query ={};
+
+      if(req.query.email){
+        query ={
+          email: req.query.email
+        }
+      }
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/orders', async(req, res)=>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
+    })
+
+    app.delete('/orders/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
     })
    
     await client.db("admin").command({ ping: 1 });
