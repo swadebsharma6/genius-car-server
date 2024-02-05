a5cc3670f9fa564720f5cee34fd6d3171729dcd6
swadebsharma6 <swadebriva2012@gmail.com>
2024-02-05 21:53:19 +0600

    added team api

--- index.js
+++ index.js
@@ -29,12 +29,19 @@ async function run() {
     const serviceCollection = client.db('GeniusCarDB').collection('services');
     const productCollection = client.db('GeniusCarDB').collection('products');
     const orderCollection = client.db('GeniusCarDB').collection('orders');
+    const teamCollection = client.db('GeniusCarDB').collection('team');
 
     app.get('/services', async(req, res)=>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     });
+
+    app.get('/team', async(req, res)=>{
+      const cursor = teamCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     app.get('/services/:id', async(req, res)=>{
       const id = req.params.id;
@@ -66,6 +73,19 @@ async function run() {
     app.post('/orders', async(req, res)=>{
       const order = req.body;
       const result = await orderCollection.insertOne(order);
+      res.send(result)
+    })
+
+    app.patch('/orders/:id', async(req, res)=>{
+      const id = req.params.id;
+      const status = req.body.status;
+      const query ={_id : new ObjectId(id)}
+      const updateDoc ={
+        $set:{
+          status: status
+        }
+      };
+      const result = await orderCollection.updateOne(query, updateDoc);
       res.send(result)
     })
 
